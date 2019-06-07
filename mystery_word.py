e4de2cf246a65fdae11be7e97bd6f54b8271bfff
David Barnes <david.barnes115@gmail.com>
2019-06-07 09:26:47 -0400

    additional update to python program

--- mystery_word.py
+++ mystery_word.py
@@ -2,7 +2,7 @@ import random
 import re
 
 f = open("words.txt")
-word_list = (f.read())
+word_list = (f.read()).lower()
 split_word_list = word_list.split()
 
 # print(random.choice(split_word_list))
@@ -25,21 +25,45 @@ user_choice = input("Which mode would you like to play on: easy, normal, or hard
 
 user_choice_lower_case = user_choice.lower()
 
+# while user_choice != "easy" or "normal" or "hard":
+while user_choice_lower_case not in ["easy", "normal", "hard"]:
+    print("Invalid Entry.  Please check your spelling and try again")
+    user_choice = input("Which mode would you like to play on: easy, normal, or hard? ")
+
+user_choice_lower_case = user_choice.lower()
+
+
 if user_choice_lower_case == "easy":
-    easy_choice = random.choice(easy_words)
-    print ("This words is", len(easy_choice), "characters long.")
+    random_word = random.choice(easy_words)
+    print ("This words is", len(random_word), "characters long.")
 elif user_choice_lower_case == "normal":
-    normal_choice = random.choice(normal_words)
-    print ("This word is", len(normal_choice), "characters long.")
+    random_word = random.choice(normal_words)
+    print ("This word is", len(random_word), "characters long.")
 elif user_choice_lower_case == "hard":
-    hard_choice = random.choice(hard_words)
-    print ("This word is", len(hard_choice),  "characters long.")
+    random_word = random.choice(hard_words)
+    print ("This word is", len(random_word),  "characters long.")
+# elif user_choice_lower_case != "easy" or "normal" or "hard":
+#     print ("Please check your spelling and try again.")
+
+word = random_word
+current_guesses = []
 
 first_letter_choice = input("Please enter one letter: ").lower()
 
+if len(first_letter_choice) == 1:
+    current_guesses.append(first_letter_choice)
+
 while len(first_letter_choice) != 1:
     print("Invalid entry - please try again.  Remember to type only one letter with each guess.")
-    first_letter_choice= input("Please enter one letter: ").lower()
+    first_letter_choice = input("Please enter one letter: ").lower()
+
+
+
+
+
+
+
+
 
 
 
